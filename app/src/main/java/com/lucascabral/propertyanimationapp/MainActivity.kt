ef1f72d5670bb82bbc01af5bf4d0fd40cc7b9fa2
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-06-21 18:20:48 -0300

    init commit

--- app/src/main/java/com/lucascabral/propertyanimationapp/MainActivity.kt
+++ app/src/main/java/com/lucascabral/propertyanimationapp/MainActivity.kt
@@ -2,10 +2,64 @@ package com.lucascabral.propertyanimationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lucascabral.propertyanimationapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        listenersToClick()
+    }
+
+    private fun listenersToClick() {
+        binding.apply {
+            rotateButton.setOnClickListener {
+                rotater()
+            }
+            translateButton.setOnClickListener {
+                translater()
+            }
+            scaleButton.setOnClickListener {
+                scaler()
+            }
+            fadeButton.setOnClickListener {
+                fader()
+            }
+            colorizeButton.setOnClickListener {
+                colorizer()
+            }
+            showerButton.setOnClickListener {
+                shower()
+            }
+        }
+    }
+
+    private fun rotater() {
+
+    }
+
+    private fun translater() {
+
+    }
+
+    private fun scaler() {
+
+    }
+
+    private fun fader() {
+
+    }
+
+    private fun colorizer() {
+
+    }
+
+    private fun shower() {
+
     }
 }
