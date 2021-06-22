4bca14eb7c4be750fdf877c8966922a75a50be25
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-06-22 16:48:49 -0300

    animação showerStars finalizada.

--- app/src/main/java/com/lucascabral/propertyanimationapp/MainActivity.kt
+++ app/src/main/java/com/lucascabral/propertyanimationapp/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.lucascabral.propertyanimationapp
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.animation.ObjectAnimator
-import android.animation.PropertyValuesHolder
+import android.animation.*
 import android.annotation.SuppressLint
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.LinearInterpolator
+import android.widget.FrameLayout
+import androidx.appcompat.widget.AppCompatImageView
 import com.lucascabral.propertyanimationapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -41,7 +43,7 @@ class MainActivity : AppCompatActivity() {
                 colorizeBackground()
             }
             showerButton.setOnClickListener {
-                shower()
+                showerStars()
             }
         }
     }
@@ -83,8 +85,10 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("ObjectAnimatorBinding")
     private fun colorizeBackground() {
-        val animator = ObjectAnimator.ofArgb(binding.star.parent,
-            "backgroundColor", Color.BLACK, Color.RED)
+        val animator = ObjectAnimator.ofArgb(
+            binding.star.parent,
+            "backgroundColor", Color.BLACK, Color.CYAN
+        )
         animator.duration = 500
         animator.repeatCount = 1
         animator.repeatMode = ObjectAnimator.REVERSE
@@ -92,8 +96,43 @@ class MainActivity : AppCompatActivity() {
         animator.start()
     }
 
-    private fun shower() {
+    private fun showerStars() {
+        val container = binding.star.parent as ViewGroup
+        val containerWidth = container.width
+        val containerHeight = container.height
+        var starWidth: Float = binding.star.width.toFloat()
+        var starHeight: Float = binding.star.height.toFloat()
 
+        val newStar = AppCompatImageView(this)
+        newStar.setImageResource(R.drawable.ic_star_24)
+        newStar.layoutParams = FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.WRAP_CONTENT,
+            FrameLayout.LayoutParams.WRAP_CONTENT
+        )
+        container.addView(newStar)
+        newStar.scaleX = Math.random().toFloat() * 1.5f + .1f
+        newStar.scaleY = newStar.scaleX
+        starWidth *= newStar.scaleX
+        starHeight *= newStar.scaleY
+        newStar.translationX = Math.random().toFloat() * containerWidth - starWidth / 2
+
+        val mover = ObjectAnimator
+            .ofFloat(newStar, View.TRANSLATION_Y, -starHeight, containerHeight + starHeight)
+        mover.interpolator = AccelerateInterpolator(1f)
+
+        val rotator = ObjectAnimator.ofFloat(newStar, View.ROTATION,
+            (Math.random() * 1080).toFloat())
+        rotator.interpolator = LinearInterpolator()
+
+        val set = AnimatorSet()
+        set.playTogether(mover, rotator)
+        set.duration = (Math.random() * 1500 + 500).toLong()
+        set.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                container.removeView(newStar)
+            }
+        })
+        set.start()
     }
 
     private fun ObjectAnimator.disableViewDuringAnimation(view: View) {
